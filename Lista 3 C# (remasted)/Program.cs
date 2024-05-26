5b29a7d9fb76a3d6e6b9a9db62b4c1c28e0379a8
Renanzin <renan.santos17senai@gmail.com>
2024-05-26 15:55:16 -0300

    a

---
+++ Lista 3 C# (remasted)/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+
+namespace Lista_3_C___remasted_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*ATIVIDADE 1*/
+
+            /*int nFuncionario;
+            double hTrabalhadas, valorHora, salario;
+
+            Console.Write("Numero do funcionario: ");
+            nFuncionario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Horas Trabalhadas: ");
+            hTrabalhadas = double.Parse(Console.ReadLine());
+
+            Console.Write("Valor por hora: ");
+            valorHora = double.Parse(Console.ReadLine());
+
+            salario = hTrabalhadas * valorHora;
+
+            Console.WriteLine("Numero do funcionario :" +  nFuncionario);
+            Console.WriteLine("Salario do funcionario :" + salario);*/
+
+
+            /*ATIVIDADE 2*/
+
+            /*Console.Write("Digite o código da peça 1: ");
+            int codigoPeca1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o número da peça 1: ");
+            int numeroPecas1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valorda peça 1: ");
+            double valorUnitarioPeca1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o código da peça 2: ");
+            int codigoPeca2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o número de peças 2: ");
+            int numeroPecas2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor da peça 2: ");
+            double valorUnitarioPeca2 = double.Parse(Console.ReadLine());
+
+            double valorTotalPeca1 = numeroPecas1 * valorUnitarioPeca1;
+            double valorTotalPeca2 = numeroPecas2 * valorUnitarioPeca2;
+            double valorTotal = valorTotalPeca1 + valorTotalPeca2;
+
+            Console.WriteLine($"Valor total a ser pago: {valorTotal:F2}");*/
+
+
+
+
+
+
+        }
+    }
+}
